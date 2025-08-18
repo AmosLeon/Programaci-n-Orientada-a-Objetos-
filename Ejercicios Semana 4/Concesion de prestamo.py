f169dad6f5d9b399345c7b837b21fd828a4b4ed2
AmósLeón <leonamos65@gmail.com>
2025-08-18 15:59:52 -0600

    Guia

--- Ejercicios Semana 4/Concesion de prestamo.py
+++ Ejercicios Semana 4/Concesion de prestamo.py
@@ -1,6 +1,8 @@
+# Solicita el salario mensaual y los años trabajados
 salario = float(input("Ingrese su salario mensual: "))
 años = int(input("Ingrese los años trabajados: "))
 
+# Verifica si el préstamo puede ser aprobado, si no, lo deniega
 if salario > 30000 or años >= 2:
     print("Prestamo aprobado.")
 else:
--- Ejercicios Semana 4/Ingreso a una Pelicula.py
+++ Ejercicios Semana 4/Ingreso a una Pelicula.py
@@ -1,10 +1,15 @@
+# Solicita al usuario que ingrese su edad
 edad = int(input("Ingrese su edad: "))
 
+# Si la edad es 18 o más, permite el ingreso sin más preguntas
 if edad >= 18:
     print("Puedes ingresar a ver la pelicula.")
 else:
+    # Si es menor de 18, pregunta si viene con un adulto acompañante
     adulto_acompañante = input("Vienes con un adulto acompañante? (si/no): ").lower()
+    # Si viene con acompañante, permite el ingreso
     if adulto_acompañante == "si":
         print("Puedes ingresar a ver la pelicula.")
     else:
+        # Si no viene con acompañante, no permite el ingreso
         print("No tienes permitido ingresar a ver la pelicula.")
